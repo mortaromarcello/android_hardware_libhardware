d511c244baa3cd4d308b32baea30198c7cbdc7af
mortaromarcello <mortaromarcello@gmail.com>
2012-10-06 09:39:54 +0200

    modified:   hardware.c 	modified:   include/hardware/audio.h 	modified:   include/hardware/gralloc.h 	modified:   include/hardware/hardware.h 	modified:   include/hardware/hwcomposer.h

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -131,7 +131,8 @@ __BEGIN_DECLS
 
 #define AUDIO_PARAMETER_RAW_DATA_OUT "raw_data_output"
 
-
+/* Query if a2dp  is supported */
+#define AUDIO_PARAMETER_KEY_HANDLE_A2DP_DEVICE "isA2dpDeviceSupported"
 /**************************************/
 
 /* common audio stream configuration parameters */
@@ -142,6 +143,9 @@ struct audio_config {
 };
 
 typedef struct audio_config audio_config_t;
+#ifdef QCOM_HARDWARE
+typedef struct buf_info;
+#endif
 
 /* common audio stream parameters and operations */
 struct audio_stream {
@@ -266,7 +270,6 @@ struct audio_stream_out {
     int (*get_render_position)(const struct audio_stream_out *stream,
                                uint32_t *dsp_frames);
 
-#ifndef ICS_AUDIO_BLOB
 #ifdef QCOM_HARDWARE
     /**
      * start audio data rendering
@@ -297,15 +300,70 @@ struct audio_stream_out {
                                     int64_t *timestamp);
 #ifdef QCOM_HARDWARE
     /**
-    * EOS notification from HAL to Flinger
+    * return the current timestamp after quering to the driver
+     */
+    int (*get_time_stamp)(const struct audio_stream_out *stream,
+                               uint64_t *time_stamp);
+    /**
+    * EOS notification from HAL to Player
      */
     int (*set_observer)(const struct audio_stream_out *stream,
                                void *observer);
-#endif
+    /**
+     * Get the physical address of the buffer allocated in the
+     * driver
+     */
+    int (*get_buffer_info) (const struct audio_stream_out *stream,
+                                struct buf_info **buf);
+    /**
+     * Check if next buffer is available. Waits until next buffer is
+     * available
+     */
+    int (*is_buffer_available) (const struct audio_stream_out *stream,
+                                     int *isAvail);
 #endif
 
 };
 typedef struct audio_stream_out audio_stream_out_t;
+
+#ifdef QCOM_HARDWARE
+/**
+ * audio_broadcast_stream is the abstraction interface for the
+ * audio output hardware.
+ *
+ * It provides information about various properties of the audio output
+ * hardware driver.
+ */
+
+struct audio_broadcast_stream {
+    struct audio_stream common;
+
+    /**
+     * return the audio hardware driver latency in milli seconds.
+     */
+    uint32_t (*get_latency)(const struct audio_broadcast_stream *stream);
+
+    /**
+     * Use this method in situations where audio mixing is done in the
+     * hardware. This method serves as a direct interface with hardware,
+     * allowing you to directly set the volume as apposed to via the framework.
+     * This method might produce multiple PCM outputs or hardware accelerated
+     * codecs, such as MP3 or AAC.
+     */
+    int (*set_volume)(struct audio_broadcast_stream *stream, float left, float right);
+
+    int (*mute)(struct audio_broadcast_stream *stream, bool mute);
+
+    int (*start)(struct audio_broadcast_stream *stream, int64_t absTimeToStart);
+    /**
+     * write audio buffer to driver. Returns number of bytes written
+     */
+    ssize_t (*write)(struct audio_broadcast_stream *stream, const void* buffer,
+                     size_t bytes, int64_t timestamp, int audioType);
+
+};
+typedef struct audio_broadcast_stream audio_broadcast_stream_t;
+#endif
 
 struct audio_stream_in {
     struct audio_stream common;
@@ -343,6 +401,8 @@ static inline size_t audio_stream_frame_size(struct audio_stream *s)
     size_t chan_samp_sz;
     uint32_t chan_mask = s->get_channels(s);
     int format = s->get_format(s);
+    char *tmpparam;
+    int isParamEqual;
 
 #ifdef QCOM_HARDWARE
     if (audio_is_input_channel(chan_mask)) {
@@ -350,7 +410,10 @@ static inline size_t audio_stream_frame_size(struct audio_stream *s)
                       AUDIO_CHANNEL_IN_MONO );
     }
 
-    if(!strcmp(s->get_parameters(s, "voip_flag"),"voip_flag=1")) {
+    tmpparam = s->get_parameters(s, "voip_flag");
+    isParamEqual = !strcmp(tmpparam,"voip_flag=1");
+    free(tmpparam);
+    if(isParamEqual) {
         if(format != AUDIO_FORMAT_PCM_8_BIT)
             return popcount(chan_mask) * sizeof(int16_t);
         else
@@ -501,6 +564,19 @@ struct audio_hw_device {
     void (*close_output_stream)(struct audio_hw_device *dev,
                                 struct audio_stream_out* stream_out);
 
+#ifdef QCOM_HARDWARE
+    /** This method creates and opens the audio hardware output
+     *  for broadcast stream */
+    int (*open_broadcast_stream)(struct audio_hw_device *dev, uint32_t devices,
+                                 int format, uint32_t channels,
+                                 uint32_t sample_rate,
+                                 uint32_t audio_source,
+                                 struct audio_broadcast_stream **out);
+
+    void (*close_broadcast_stream)(struct audio_hw_device *dev,
+                                   struct audio_broadcast_stream *out);
+#endif
+
     /** This method creates and opens the audio hardware input stream */
 #ifndef ICS_AUDIO_BLOB
     int (*open_input_stream)(struct audio_hw_device *dev,
@@ -539,6 +615,14 @@ static inline int audio_hw_device_close(struct audio_hw_device* device)
 }
 
 #ifdef QCOM_HARDWARE
+/** Structure to save buffer information for applying effects for
+ *  LPA buffers */
+struct buf_info {
+    int bufsize;
+    int nBufs;
+    int **buffers;
+};
+
 #ifdef __cplusplus
 /**
  *Observer class to post the Events from HAL to Flinger
