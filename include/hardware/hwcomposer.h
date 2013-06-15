98746562e2cab07aa74d191751b2a88fcc453986
mortaromarcello <mortaromarcello@gmail.com>
2013-06-15 10:53:17 +0200

    modified:   include/hardware/audio.h 	modified:   include/hardware/audio_policy.h 	modified:   include/hardware/fb.h 	modified:   include/hardware/hwcomposer.h

--- include/hardware/hwcomposer.h
+++ include/hardware/hwcomposer.h
@@ -388,6 +388,19 @@ typedef struct hwc_module {
     struct hw_module_t common;
 } hwc_module_t;
 
+#ifdef STE_HARDWARE
+/*
+ * names for setParameter()
+ */
+enum {
+    /* Specifies the UI orientation */
+    HWC_UI_ORIENTATION = 0x00000000,
+    /* Specifies if hardware rotation is used */
+    HWC_HARDWARE_ROTATION = 0x00000001,
+    /* Set the hdmi plug status */
+    HWC_HDMI_PLUGGED = 0x00000002,
+};
+#endif
 
 typedef struct hwc_composer_device {
     struct hw_device_t common;
@@ -507,6 +520,18 @@ typedef struct hwc_composer_device {
      * availability: HWC_DEVICE_API_VERSION_0_3
      */
     hwc_methods_t const *methods;
+
+#ifdef STE_HARDWARE
+  /*
+     * This hook is vendor specific and optional.
+     *
+     * (*setParameter)() makes the hardware composer aware of the system state,
+     * e.g. hdmi plug status and ui rotation, so that it can make intelligent
+     * decisions on how to handle composed surfaces and cloning in the kernel.
+     */
+    int (*setParameter)(struct hwc_composer_device* dev,
+                int param, int value);
+#endif
 
     int         (*setparameter)(struct hwc_composer_device* dev,uint32_t cmd,uint32_t value);
     uint32_t    (*getparameter)(struct hwc_composer_device* dev,uint32_t cmd);
